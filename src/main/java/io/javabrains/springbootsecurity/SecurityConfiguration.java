e9382d8ecdd6194dc1f16c05d4824f56fa0aa621
Koushik Kothagal <koush.s@gmail.com>
2019-08-21 15:11:28 -0700

    Configuring authentication

---
+++ src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package io.javabrains.springbootsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Set your configuration on the auth object
+        auth.inMemoryAuthentication()
+                .withUser("blah")
+                .password("blah")
+                .roles("USER")
+                .and()
+                .withUser("foo")
+                .password("foo")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
